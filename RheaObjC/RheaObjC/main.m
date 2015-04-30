45894c08c64f1721bedcdf59bd7216da6bcde1d9
Balaji Ravindran <Balaji@Balajis-MacBook-Pro.local>
2015-04-29 23:26:38 -0700

    First Commit to Rhea

---
+++ RheaObjC/RheaObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RheaObjC
+//
+//  Created by Balaji Ravindran on 4/29/15.
+//  Copyright (c) 2015 Balaji Ravindran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
